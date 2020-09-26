49a7b0827f6d87e3a0662f87723ad8045bcf7829
Sabro98 <inhyuk04@gmail.com>
2020-09-26 18:03:22 +0900

    loop camera

--- main.py
+++ main.py
@@ -7,23 +7,24 @@ from buttonCamera import Camera
 DIR_PATH = './resources'
 FILE_NAME = 'test.jpg'
 camera = Camera(DIR_PATH, FILE_NAME)
-camera.run()
 reader = Reader(DIR_PATH, FILE_NAME)
 speech = Speech()
-words = reader.run().split('\n')[:-1]
+while True:
+    camera.run()
+    words = reader.run().split('\n')[:-1]
 
-for i in range(len(words)):
-    word = words[i]
-    print(word)
-    img = Image.new('RGB', (len(word) * 45, 80), color = (0, 0, 0))
-    d = ImageDraw.Draw(img)
-    fnt = ImageFont.truetype('./Font.ttf', 50)
-    d.text((10, 10), word, font = fnt, fill = (255, 255, 255))
-    FILE_NAME = 'test{}.png'.format(i)
-    img.save(FILE_NAME)
-    # subprocess.call(['open', FILE_NAME])
-    speech.run(word)
-    camera.getBtn().wait_for_press()
+    for i in range(len(words)):
+        word = words[i]
+        print(word)
+        img = Image.new('RGB', (len(word) * 45, 80), color = (0, 0, 0))
+        d = ImageDraw.Draw(img)
+        fnt = ImageFont.truetype('./Font.ttf', 50)
+        d.text((10, 10), word, font = fnt, fill = (255, 255, 255))
+        FILE_NAME = 'test{}.png'.format(i)
+        img.save(FILE_NAME)
+        # subprocess.call(['open', FILE_NAME])
+        speech.run(word)
+        camera.getBtn().wait_for_press()
 
-for i in range(len(words)):
-    remove('file{}.png'.format(i))
+    for i in range(len(words)):
+        remove('file{}.png'.format(i))
