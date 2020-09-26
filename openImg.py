83be5aecf1793d1402514f6dba01c1a26cb00bf2
Sabro98 <inhyuk04@gmail.com>
2020-09-26 19:31:42 +0900

    thread

--- openImg.py
+++ openImg.py
@@ -1,5 +1,12 @@
 import subprocess
-try:
-    subprocess.run(['feh', '-YF', 'resources/test.jpg'], timeout=3)
-except:
-    print('openImg ended')
+import threading
+
+def runMP3(speech, word):
+    speech.run(word)
+def showim(fileName, speech, word):
+    try:
+        thread = threading.Thread(target = runMP3, args=(i,))
+        thread.start()
+        subprocess.run(['feh', '-YF', fileName], timeout=3)
+    except:
+        print('openImg ended')
