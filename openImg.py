cb5462a0b936b6d6c418301a92c2b38a32af53aa
Sabro98 <inhyuk04@gmail.com>
2020-09-26 20:32:27 +0900

    commit

--- openImg.py
+++ openImg.py
@@ -4,16 +4,15 @@ import psutil
 from buttonCamera import Camera
 
 PROCNAME = "feh"
-def runMP3(speech, word):
-    speech.run(word)
-def showImg(fileName):
-    subprocess.run(['feh', '-YF', fileName], timeout=100)
+FILENAME = ''
+def showImg():
+    subprocess.run(['feh', '-YF', FILENAME], timeout=100)
 
 def showim(fileName, speech, word):
     try:
-        # thread = threading.Thread(target = runMP3, args=(speech, word))
-        # thread.start()
-        imgThread = threading.Thread(target = showImg, args = (fileName))
+        global FILENAME
+        FILENAME = fileName
+        imgThread = threading.Thread(target = showImg)
         imgThread.start()
         speech.run(word)
     except:
